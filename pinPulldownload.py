789ee510d3af351ab42efa601bdfc6ea0353e8e4
Phil DeSantis <phildewpoint@gmail.com>
2018-04-24 16:52:04 -0500

    Update to tree structure

--- pinPulldownload.py
+++ pinPulldownload.py
@@ -1,7 +1,7 @@
 # this app takes a return delimited file (like a column from an Excel spreadsheet) and parses apart all of the
 # related information from the cook county assessor's office
 
-import bs4, requests, csv, selenium, re
+import bs4, requests, csv, selenium, re, time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -51,17 +51,16 @@ property_char = {
 def main():
     # create driver object, get the URL
     driver = selenium.webdriver.Chrome()
+    driver.implicitly_wait(10)
     driver.get(url)
     pinList = []
     next_page = 1
-    i = 1
     # checks to make sure the page is correct, if so continues, if not quits
-    while (next_page != "") or (i < 20):
+    while (next_page !=""):
         try:
-            element = WebDriverWait(driver, 60).until(
+            WebDriverWait(driver, 60).until(
                 EC.presence_of_element_located((By.ID,"ctl00_phArticle_ctlPropertySearch_ctlSearchResults_pnlAppealSearchResults"))
             )
-            next_page = driver.find_element_by_link_text("Next")
         except:
             print("Unable to find element")
             driver.quit()
@@ -72,65 +71,62 @@ def main():
             pinList.append(link.text)
 
         # selenium goes to next page
-        i+ = 1
         try:
+            next_page = driver.find_element_by_link_text("Next")
             next_page.click()
-        except:
-            print("No next page, quitting")
-            driver.quit()
-            quit()
-    quit()
+            time.sleep(5)
+        except selenium.common.exceptions.NoSuchElementException:
+            print("No next page, all set")
+            next_page = ""
+            pass
+    # create CSV file to store home info
+    csv_file = open(file='testFile.csv', mode='w', newline='')
+    # loop through each PIN and pull the HTML info into the CSV. Use enumerate to track loops
+    for i, pins in enumerate(pinList):
+        # create an empty dictionary for tracking
+        all_keys = {}
+        pin_dict = {
+            'pin': pins
+        }
+        # pull home info down based on PIN
+        home = requests.get(url=url, params=pin_dict)
+        home.raise_for_status()
+        soup = bs4.BeautifulSoup(markup=home.text, features='html.parser')
 
-    # # pull listing of pins from the file
-    # pinList = open('pinListing', mode='r').read()
-    # # create CSV file to store home info
-    # csv_file = open(file='testFile.csv', mode='w', newline='')
-    # # loop through each PIN and pull the HTML info into the CSV. Use enumerate to track loops
-    # for i, pins in enumerate(pinList.split()):
-    #     # create an empty dictionary for tracking
-    #     all_keys = {}
-    #     pin_dict = {
-    #         'pin': pins
-    #     }
-    #     # pull home info down based on PIN
-    #     home = requests.get(url=url, params=pin_dict)
-    #     home.raise_for_status()
-    #     soup = bs4.BeautifulSoup(markup=home.text, features='html.parser')
+        # print out the 'Property Information' section
+        for k, v in property_info.items():
+            val = soup.find(id=v)
+            try:
+                all_keys[k] = val.string
+            except AttributeError:
+                all_keys[k] = 'N/a'
 
-    #     # print out the 'Property Information' section
-    #     for k, v in property_info.items():
-    #         val = soup.find(id=v)
-    #         try:
-    #             all_keys[k] = val.string
-    #         except AttributeError:
-    #             all_keys[k] = 'N/a'
+        # print out the 'Assessed Valuation' section
+        for k, v in assessed_valuation.items():
+            val = soup.find(id=v)
+            try:
+                all_keys[k] = val.string
+            except AttributeError:
+                all_keys[k] = 'N/a'
 
-    #     # print out the 'Assessed Valuation' section
-    #     for k, v in assessed_valuation.items():
-    #         val = soup.find(id=v)
-    #         try:
-    #             all_keys[k] = val.string
-    #         except AttributeError:
-    #             all_keys[k] = 'N/a'
+        # print out the 'Property Characteristics' section
+        for k, v in property_char.items():
+            val = soup.find(id=v)
+            try:
+                all_keys[k] = val.string
+            except AttributeError:
+                all_keys[k] = 'N/a'
 
-    #     # print out the 'Property Characteristics' section
-    #     for k, v in property_char.items():
-    #         val = soup.find(id=v)
-    #         try:
-    #             all_keys[k] = val.string
-    #         except AttributeError:
-    #             all_keys[k] = 'N/a'
-
-    #     # add the 'clean pin' without dashes into the dict
-    #     all_keys['Num-Only Pin'] = all_keys.get('Pin').replace('-', '')
-    #     if i == 0:
-    #         # write a header section
-    #         mycsv = csv.DictWriter(f=csv_file, fieldnames=all_keys.keys(), delimiter='|')
-    #         mycsv.writeheader()
-    #     else:
-    #         mycsv.writerow(rowdict=all_keys)
-    # csv_file.close()
-    # print('All set!')
+        # add the 'clean pin' without dashes into the dict
+        all_keys['Num-Only Pin'] = all_keys.get('Pin').replace('-', '')
+        if i == 0:
+            # write a header section
+            mycsv = csv.DictWriter(f=csv_file, fieldnames=all_keys.keys(), delimiter='|')
+            mycsv.writeheader()
+        else:
+            mycsv.writerow(rowdict=all_keys)
+    csv_file.close()
+    print('All set!')
 
 
 if __name__ == '__main__':
